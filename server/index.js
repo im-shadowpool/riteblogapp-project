539a859e725eb390f1509ca896dbd2dc2e833ea4
Saipavan Veeravalli <shadowpoolvs@gmail.com>
2024-02-09 16:28:49 +0530

    cors updated

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const { connect } = require("mongoose");
 require("dotenv").config();
 const upload = require('express-fileupload')
+var favicon = require('serve-favicon')
 
 const userRoutes = require("./Routes/usersRoutes");
 const postsRoutes = require("./Routes/postsRoutes");
@@ -11,17 +12,20 @@ const { notFound, errorHandlers } = require('./middlewares/errorMiddlewares')
 const app = express();
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors({ credentials: true, origin: "https://riteblogapp-project-vmy7.vercel.app" }));
 
 app.use(upload())
 app.use('/uploads', express.static(__dirname + '/uploads'))
 
+
+app.use(favicon(path.join(__dirname, + '/favicon.ico')))
 
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postsRoutes);
 
 app.use(notFound)
 app.use(errorHandlers)
+
 
 
 connect(process.env.MONGO_URI)
